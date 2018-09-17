4bdb6ad8cb47cb6065fdf8a5c29cf5effec5e595
Shawna Davis <shawna_davis@alum.mit.edu>
2018-09-17 20:00:55 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,24 @@
 # Write your code here!
 
+def game_hash
+game_hash = {
+ :home => {
+   :team_name => " ",
+   :colors => [],
+   :players => {
+     :name => {
+       :number => ,
+       :shoe => ,
+       :points => ,
+     }
+   }
+ }
+ :away =>{
+   
+ }
 
+}  
+end
 
 
 
